59ffb0f8d12b027c371f80fff815d50648dc68da
Jonathan Eid <jonathaneid98@gmail.com>
2021-01-22 15:56:56 -0500

    Fixing package names

---
+++ src/main/java/zapprotocol/jzap/JZapApplication.java
@@ -0,0 +1,13 @@
+package zapprotocol.jzap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JZapApplication.class, args);
+	}
+
+}
