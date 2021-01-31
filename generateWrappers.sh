2da4c8f894b403ce3feaac5b76c103967d5ae468
Jonathan-Eid <jonathaneid98@gmail.com>
2021-01-31 17:51:56 -0500

    Update generateWrappers.sh

--- generateWrappers.sh
+++ generateWrappers.sh
@@ -1,8 +1,8 @@
 generateWrapper(){
     echo "Generating Wrappers...."
     for FILE in build/contracts/*
-     echo "Opening $FILE"
     do
+    echo "Opening $FILE"
     fbname=$(basename "$FILE" | cut -d. -f1)
     echo "CHECKING TARGET FOLDER"
     if (ls ./target/classes/io/github/zapproject/jzap/wrappers |  grep -q  "$fbname.java")
