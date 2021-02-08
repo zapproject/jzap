d04fc2d4e862a84acc02798616fa010ea4416621
Robin Lopez <andrelopez41@live.com>
2021-02-08 14:00:26 -0500

    Added assertion to unit test

--- src/test/java/io/github/zapproject/jzap/Curve/curveTest.java
+++ src/test/java/io/github/zapproject/jzap/Curve/curveTest.java
@@ -2,6 +2,9 @@ package io.github.zapproject.jzap.Curve;
 
 import org.junit.jupiter.api.Test;
 import io.github.zapproject.jzap.Curve.src.Curve;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.*;
 
 public class curveTest {
@@ -12,6 +15,10 @@ public class curveTest {
     @Test
     void init() {
         testCurve = new Curve(testValues);
+        testValues.add(5);
+        testValues.add(3);
+        testValues.add(12);
+        assertEquals(testCurve.values.size(), 3);
     }
     
 }
