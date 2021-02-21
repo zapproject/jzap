c6b7c2b166a8c31958df1eaeae3b61a25017ec40
Robin Lopez <andrelopez41@live.com>
2021-02-20 20:56:02 -0500

    Added additional methods to Curve class

--- src/main/java/io/github/zapproject/jzap/Curve/src/Curve.java
+++ src/main/java/io/github/zapproject/jzap/Curve/src/Curve.java
@@ -1,5 +1,6 @@
 package io.github.zapproject.jzap.Curve.src;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Curve {
@@ -36,4 +37,112 @@ public class Curve {
         }
         this.max = prevEnd;
     }
+
+    public int getPrice(int total_x) {
+        if (total_x <= 0 || total_x > this.max) {
+            throw new ArithmeticException("Invalid curve supply position");
+        }
+        
+        if (this.values.size() == 0) {
+            throw new ArithmeticException("Curve is now initialized");
+        }
+
+        int index = 0;
+        while (index < this.values.size()) {
+            int len = this.values.get(index);
+            int end = this.values.get(index+len+1);
+
+            if (total_x > end) {
+                index += len + 2;
+                continue;
+            }
+
+            int sum = 0;
+            for(int i = 0; i<len;i++) {
+                int coeff = this.values.get(index+i+1);
+                sum += coeff * Math.pow(total_x, i);
+            }
+
+            return sum;
+        }
+
+        return -1;
+    }
+
+    public int getZapRequired(int a, int n) {
+        int sum = 0;
+        for(int i=0;i<a+n;i++) {
+            sum += this.getPrice(i);
+        }
+        return sum;
+    }
+
+    public void convertToBNArrays() {
+        this.values.stream().map(number -> {
+            BigInteger num = new BigInteger(Integer.toString(number));
+            return num;
+        });
+    }
+
+    public List<String> valuesToString() {
+        List<String> myList = new ArrayList<String>();
+        this.values.forEach(n -> myList.add(Integer.toString(n)));
+        return myList;
+    }
+
+    public static String curveToString(List<Integer> values) {
+        List<List<Integer>> items = Curve.splitCurveToTerms(values);
+        List<String> stringItems = new ArrayList<String>();
+
+        items.stream().map(term -> {
+            return stringItems.add(Curve.termToString(term));
+        });
+
+        return String.join(" & ", stringItems);
+    }
+
+    private static List<List<Integer>> splitCurveToTerms(List<Integer> curve) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        if (curve.size() <= 0) {
+            return res;
+        }
+
+        int startIndex = 0;
+        int currentLength = curve.get(0);
+        int endIndex = currentLength + 2;
+
+        while(startIndex < curve.size()) {
+            res.add(curve.subList(startIndex, endIndex));
+            startIndex += 2;
+            currentLength = curve.get(endIndex);
+            endIndex = startIndex + currentLength + 2;
+        }
+
+        return res;
+    }
+
+    public static String termToString(List<Integer> term) {
+        int limit = term.get(term.size() - 1);
+        List<String> parts = new ArrayList<String>();
+
+        for(int i =1; i <= term.get(0); i++) {
+            if(term.get(i) == 0) {
+                continue;
+            }
+
+            if(term.get(i) == 1) {
+                parts.add("x^" + (i-1));
+            } else {
+                parts.add(term.get(i) + "*" + "x^" + (i-1));
+            } 
+        }
+
+        return String.join("+", parts) + "; limit = " + limit;
+    }
+
+    public static List<Integer> converToCurve(int end, String curve) {
+        return ""
+    }
+
 }
